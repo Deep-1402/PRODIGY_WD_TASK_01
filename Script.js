84feee3de132f5606ab743c8ae51f93fab415b28
Patel Deep <PatelDeep873873@gmail.com>
2024-06-16 22:01:35 +0530

    done

--- Script.js
+++ Script.js
@@ -3,8 +3,10 @@ function ChangeFont(){
 }
 
 function ChangeBackGround(){
-    document.getElementById("bd").style.backgroundImage = "url('BackGround1.jpg') ";
+    // document.body.style.background = "url('img_tree.png')";
+    // document.getElementById("bd").style.background = "#f3f3f3 url('BackGround1.jpg')  no-repeat auto";
     document.getElementById("bd").style.backgroundRepeat = "no-repeat";
     document.getElementById("bd").style.backgroundSize = "auto";
+    document.getElementById("bd").style.backgroundImage = "url('BackGround1.jpg') ";
 }
 
