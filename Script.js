97571a6b2dabd45b5c3d4db78d1614c250d7037d
Patel Deep <121955130+Deep-1402@users.noreply.github.com>
2024-06-16 21:56:30 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -3,10 +3,6 @@ function ChangeFont(){
 }
 
 function ChangeBackGround(){
-    // document.body.style.background = "url('img_tree.png')";
-    // document.getElementById("bd").style.background = "#f3f3f3 url('BackGround1.jpg')  no-repeat auto";
-    document.getElementById("bd").style.backgroundRepeat = "no-repeat";
-    document.getElementById("bd").style.backgroundSize = "auto";
     document.getElementById("bd").style.backgroundImage = "url('BackGround1.jpg') ";
 }
 
